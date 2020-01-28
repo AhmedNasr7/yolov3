1beaa593f9446a4d5dfbcc52d6743d4d577a1c48
Abdelrahman-Bishr <abdelrahman.bishr@gmail.com>
2020-01-28 15:27:25 +0200

    width and height switching , wide gate boxes problem solved

--- utils/utils.py
+++ utils/utils.py
@@ -109,20 +109,20 @@ def weights_init_normal(m):
 def xyxy2xywh(x):
     # Convert bounding box format from [x1, y1, x2, y2] to [x, y, w, h]
     y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
-    y[:, 0] = (x[:, 0] + x[:, 2]) / 2
-    y[:, 1] = (x[:, 1] + x[:, 3]) / 2
-    y[:, 2] = x[:, 2] - x[:, 0]
-    y[:, 3] = x[:, 3] - x[:, 1]
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2       #X
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2       #Y
+    y[:, 2] = x[:, 2] - x[:, 0]             #W
+    y[:, 3] = x[:, 3] - x[:, 1]             #H
     return y
 
 
 def xywh2xyxy(x):
     # Convert bounding box format from [x, y, w, h] to [x1, y1, x2, y2]
     y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
-    y[:, 0] = x[:, 0] - x[:, 2] / 2
-    y[:, 1] = x[:, 1] - x[:, 3] / 2
-    y[:, 2] = x[:, 0] + x[:, 2] / 2
-    y[:, 3] = x[:, 1] + x[:, 3] / 2
+    y[:, 0] = x[:, 0] - x[:, 3] / 2         #leftX
+    y[:, 1] = x[:, 1] - x[:, 2] / 2         #topY
+    y[:, 2] = x[:, 0] + x[:, 3] / 2         #rightX
+    y[:, 3] = x[:, 1] + x[:, 2] / 2         #rightY
     return y
 
 
