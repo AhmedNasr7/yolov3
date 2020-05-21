07ce939e49404e6293b68378ea9b52e6fc729faf
ahmed.nasr9677@gmail.com <ahmed.nasr9677@gmail.com>
2020-05-21 23:07:46 +0200

    add new version of detector

--- detection.py
+++ detection.py
@@ -11,7 +11,7 @@ import time
 
 class Detector:
 
-    def __init__(self, source_img, cfg, weights, save_img=False):
+    def __init__(self, opt, source_img, cfg, weights, save_img=False):
         #soource_img, weilghts_path, 
         self.source_img = source_img
         img_size = 416 ## to be changed
@@ -21,6 +21,7 @@ class Detector:
         self.save_img = save_img
 
         self.model = Darknet(self.cfg, img_size)   # Initialize model
+        self.opt = opt
         
         # Load weights
         attempt_download(self.weights) 
@@ -49,9 +50,15 @@ class Detector:
         self.names = './cls.names'
         self.names = load_classes(self.names)
         self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(self.names))]
-        self.classes = [0, 1]
-        self.save_img = True
-        self.dataset = LoadImages(self.source_img, img_size=img_size, half=self.half)
+        self.classes = self.opt.classes
+
+        self.half = self.half and self.device.type != 'cpu'  # half precision only supported on CUDA
+
+        self.names = './cls.names'
+        self.names = load_classes(self.names)
+        self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(self.names))]
+        self.classes = None
+
 
 
     def detect(self, conf_thres = 0.3, iou_thres = 0.5):
@@ -61,81 +68,74 @@ class Detector:
         self.iou_thres = iou_thres
   
 
-        image = cv2.imread(self.source_img) ## to be changed
+        #image = cv2.imread(self.source_img) ## to be changed
 
-        #for path, img, im0s, vid_cap in self.dataset:
-        #t = time.time()
+        for path, img, im0s, vid_cap in self.dataset:
 
-        # Run inference
-        t0 = time.time()
+            t = time.time()
 
-        img = image.reshape(image.shape[0], image.shape[1], 3)
-
-        img = img.transpose((2, 0, 1))
-
-        img = np.ascontiguousarray(img, dtype=np.float16 if self.half else np.float32) # uint8 to fp16/fp32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-
-
-        img = torch.from_numpy(img).to(self.device)
-
-        with torch.no_grad():
-            pred = self.model(img.unsqueeze(dim=0))[0]
-            print("p: ", pred)
+            # Get detections
+            img = torch.from_numpy(img).to(self.device)
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+            pred = self.model(img)[0]
 
             if self.half:
                 pred = pred.float()
-                print("shalf pred", pred)
 
-            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=self.classes, agnostic=self.agnostic_nms)
-            print("nms configs: ", self.conf_thres, self.iou_thres, self.classes, self.agnostic_nms)
-            print("preds: ", pred)
-            im0 = np.copy(image)
-            s = ''
+            # Apply NMS
+            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=self.opt.classes, agnostic=self.agnostic_nms)
 
+          
+            # Process detections
             for i, det in enumerate(pred):  # detections per image
+               
+                p, s, im0 = path, '', im0s
 
-                save_path = str(Path(self.save_path) / 'output.jpg')
-                s += '%g' % img.shape[2:]
+                save_path = str(Path(self.save_path) / Path(p).name)
+                s += '%gx%g ' % img.shape[2:]  # print string
+                
                 if det is not None and len(det):
-
+                    # Rescale boxes from img_size to im0 size
                     det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
                     print('detection.shape : ',det.shape)
                     print('detection : ',det)
-
+                    # Print results
                     for c in det[:, -1].unique():
-
                         n = (det[:, -1] == c).sum()  # detections per class
                         s += '%g %ss, ' % (n, self.names[int(c)])  # add to string
 
                     # Write results
                     for *xyxy, conf, cls in det:
-                        if self.save_img or self.view_img:  # Add bbox to image
+
+                        if self.save_img:  # Add bbox to image
                             label = '%s %.2f' % (self.names[int(cls)], conf)
                             plot_one_box(xyxy, im0, label=label, color=self.colors[int(cls)])
-                            print("lbl, cls", label, cls)
 
                 # Print time (inference + NMS)
-                print('%sDone. (%.3fs)' % (s, time.time() - t0))
-
+                print('%sDone. (%.3fs)' % (s, time.time() - t))
 
                 # Save results (image with detections)
                 if self.save_img:
-                    cv2.imwrite(save_path, im0)
+                    if self.dataset.mode == 'images':
+                        cv2.imwrite(save_path, im0)
                     
-           #if self.save_img:
-                #print('Results saved to %s' % os.getcwd() + os.sep + self.save_path)
-              
 
-            print('Done. (%.3fs)' % (time.time() - t0))
+        if self.save_img:
+            print('Results saved to %s' % os.getcwd() + os.sep + self.save_path)
+        print('Done. (%.3fs)' % (time.time() - t0))
+
 
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
 	source_img = '/content/1.jpg'
 	cfg = './cfg/yolov3-spp.cfg'
 	weights = './weights/yolov3-spp.weights'
+    opt = parser.parse_args()
 
-	detector = Detector(source_img, cfg, weights, save_img=True)
+	detector = Detector(opt, source_img, cfg, weights, save_img=True)
 	detector.detect(0.99, 0.1)
 
